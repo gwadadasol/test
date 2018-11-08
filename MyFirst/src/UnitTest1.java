f6d93b6f98226768d22f9b5fa5c1387ec2c3ae90
gwadadasol <gwadadasol@gmail.com>
2018-11-08 21:26:56 +0800

    Create project MyFirst

---
+++ MyFirst/src/UnitTest1.java
@@ -0,0 +1,9 @@
+public class UnitTest1 {
+    public int addNumber(int a, int b) {
+        return a + b;
+    }
+
+    public String addString( String a , String b){
+        return a + b;
+    }
+}
---
+++ MyFirst/test/UnitTest1Test.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest1Test {
+
+    @Test
+    public void test(){
+        UnitTest1 test1 = new UnitTest1();
+        int result = test1.addNumber(5,2);
+        assertEquals(7, result);
+
+    }
+
+    @Test
+    public void testString(){
+        UnitTest1 test1 = new UnitTest1();
+        String result = test1.addString("pa", "pa");
+        assertEquals("papa", result);
+    }
+
+}
