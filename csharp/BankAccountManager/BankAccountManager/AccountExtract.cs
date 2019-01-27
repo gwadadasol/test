afe84034ec115fbba6475580aa042bdd0d40b680
pavcoding <gwadadasol@gmail.com>
2019-01-27 22:12:20 +0800

    add BankManager project

---
+++ csharp/BankAccountManager/BankAccountManager/AccountExtract.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace BankAccountManager
+{
+    internal class AccountExtract
+    {
+        public const string FILE_TO_PROCESS_DIR = @"C:\Users\phili\source\data\BankAccountManager\toprocess";
+        public const string FILE_PROCESSED_DIR = @"C:\Users\phili\source\data\BankAccountManager\processed";
+        public const string FILE_DATABASE = @"C:\Users\phili\source\data\BankAccountManager\database.csv";
+
+        public List<AccountMovement> Values { get; set; }
+
+
+        public AccountExtract()
+        {
+            Values = new List<AccountMovement>();
+        }
+
+        public void loadExtractFromCitiFile(string path)
+        {
+            var reader = new StreamReader(path);
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                List<string> values = new List<string>();
+                char comma = ',';
+
+                int index = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == comma)
+                    {
+                        if ((line[i - 1] == '"') && (line[i + 1] == '"'))
+                        {
+                            values.Add(line.Substring(index, i - index));
+                            index = i + 1;
+                        }
+                    }
+                }
+
+                values.Add(line.Substring(index, line.Length - index));
+                //var values = line.Split('~');
+
+
+                AccountMovement accountMvt = new AccountMovement();
+                string newdate = values[0].Trim('"');
+                string description = values[1].Trim('"');
+                string amount = values[2].Trim('"');
+                string account = values[4].Trim('"');
+
+                accountMvt.Date = DateTime.ParseExact(newdate, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+                accountMvt.Description = description;
+                accountMvt.Amount = Double.Parse(values[2].Trim('"'));
+                accountMvt.Account = account;
+
+                this.Values.Add(accountMvt);
+            }
+        }
+
+        public void loadExtractFromDatabase(string path)
+        {
+            var reader = new StreamReader(path);
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                var values = line.Split(';');
+                if (values.Length != 4)
+                    break;
+
+                AccountMovement accountMvt = new AccountMovement();
+                string newdate = values[0];
+                string description = values[1];
+                string amount = values[2];
+                string account = values[3];
+
+                accountMvt.Date = DateTime.ParseExact(newdate, "MM-dd-yyyy", CultureInfo.InvariantCulture);
+                accountMvt.Description = description;
+                accountMvt.Amount = Double.Parse(amount);
+                accountMvt.Account = account;
+
+                this.Values.Add(accountMvt);
+            }
+        }
+
+        public override string ToString()
+        {
+            string values = "";
+
+            foreach (AccountMovement am in Values)
+            {
+                values += am.ToString() + '\n';
+            }
+            return values;
+        }
+
+        public void ToFile(string path)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path,true))
+            {
+                file.Write(ToString());
+            }
+        }
+
+    }
+}
---
+++ csharp/BankAccountManager/BankAccountManager/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BankAccountManager
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load data from the Citibank files
+            string[] filePaths = Directory.GetFiles(AccountExtract.FILE_TO_PROCESS_DIR, "*.csv", SearchOption.TopDirectoryOnly);
+
+            AccountExtract accountExtract = new AccountExtract();
+
+            foreach (string filename in filePaths)
+            {
+                //Console.WriteLine(filename);
+                accountExtract.loadExtractFromCitiFile(filename);
+            }
+
+            //Console.Write(accountExtract);
+            Console.WriteLine("{0} movements have been loaded", accountExtract.Values.Count);
+
+            // save the data to the database
+            accountExtract.ToFile(AccountExtract.FILE_DATABASE);
+
+
+
+            // Display the database
+            AccountExtract accountExtractDB = new AccountExtract();
+            accountExtractDB.loadExtractFromDatabase(AccountExtract.FILE_DATABASE);
+            //Console.WriteLine(accountExtractDB);
+            Console.WriteLine("There are {0} movements on all accounts", accountExtractDB.Values.Count);
+        }
+    }
+}
