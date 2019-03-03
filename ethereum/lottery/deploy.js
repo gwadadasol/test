826604079cdc648d8b4e95b114e610a931301cba
gwadadasol <gwadadasol@gmail.com>
2019-03-04 07:28:58 +0800

    blockchain lottery

--- ethereum/lottery/deploy.js
+++ ethereum/lottery/deploy.js
@@ -19,8 +19,9 @@ const deploy  = async () => {
     console.log('attempting to deploy  from account', accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: [INITIAL_VARIABLE]})
+        .deploy({data: bytecode})
         .send({from: accounts[0], gas: '1000000'});
+ console.log(interface);
 
     console.log('Contract deployed to ', result.options.address);
 
