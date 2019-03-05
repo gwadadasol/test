c0f0135de553d55bace0424783856b8003f74531
gwadadasol <gwadadasol@gmail.com>
2019-03-05 23:27:24 +0800

    implementaton of client and server to display beers :)

---
+++ trainings/springfwk/bankmanagertool/client/src/GiphyImage.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+
+interface IGiphyImageProps{
+    name: string;
+}
+
+class GiphyImage extends React.Component<IGiphyImageProps, any>{
+    public constructor(props: any) {
+        super(props);
+
+        this.state = {
+            giphyUrl: '',
+            isLoading: false
+        };
+    }
+
+    public componentDidMount(): void {
+        const giphyApi = '//api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=1&q=)';
+
+        fetch(giphyApi + this.props.name)
+            .then(response => response.json())
+            .then(response => {
+                if (response.data.length > 0){
+                    this.setState({giphyUrl:response.data[0].images.original.url});
+                }else{
+                    this.setState({giphyUrl: '//media.giphy.com/media/YaOxRsmrv9IeA/giphy.gif'});
+                }
+                this.setState({isLoading: false});
+            });
+    }
+
+    public render(){
+        const {giphyUrl, isLoading} = this.state;
+
+        if (isLoading){
+            return <p>Loading image...</p>;
+        }
+
+        return(
+            <img src={giphyUrl} alt={this.props.name} width="200" />
+        );
+    }
+
+}
+export default GiphyImage;
