aa8482c5672c73049bf9c0aef28e44f7ab372d4a
gwadadasol <gwadadasol@gmail.com>
2019-05-20 07:29:21 +0800

    slight front end enhancement: Amend Grid, Typography and Paper tags. Use of colors in the list of Opereration, grid spacing...

--- trainings/springfwk/bankmanagertool/client/src/OperationList.tsx
+++ trainings/springfwk/bankmanagertool/client/src/OperationList.tsx
@@ -8,54 +8,62 @@ import 'typeface-roboto';
 import Typography from '@material-ui/core/Typography';
 import Paper from "@material-ui/core/Paper";
 
-const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    dense: {
-        marginTop: 16,
-    },
-    menu: {
-        width: 200,
-    },
-    root: {
-        marginTop: theme.spacing.unit * 3,
-        overflowX: 'auto',
-        width: '100%',
-    },
+import {withStyles} from '@material-ui/core/styles';
 
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-    },
+
+const styles = theme => ({
+    // container: {
+    //     display: 'flex',
+    //     flexWrap: 'wrap',
+    // },
+    // dense: {
+    //     marginTop: 16,
+    // },
+    // menu: {
+    //     width: 200,
+    // },
+    // root: {
+    //     marginTop: theme.spacing.unit * 3,
+    //     overflowX: 'auto',
+    //     width: '100%',
+    // },
+    // TextField: {
+    //     marginLeft: theme.spacing.unit,
+    //     marginRight: theme.spacing.unit,
+    // },
     table: {
         fontFamily: theme.typography.fontFamily,
     },
-    flexContainer: {
-        display: 'flex',
-        alignItems: 'center',
-        boxSizing: 'border-box',
-    },
+    // flexContainer: {
+    //     display: 'flex',
+    //     alignItems: 'center',
+    //     boxSizing: 'border-box',
+    // },
     tableRow: {
         cursor: 'pointer',
-    },
-    tableRowHover: {
         '&:hover': {
-            backgroundColor: theme.palette.grey[200],
+            'background-color': "red",
+            // backgroundColor: theme.palette.background.default,
         },
     },
-    tableCell: {
-        flex: 1,
-    },
-    noClick: {
-        cursor: 'initial',
-    },
-
+    // TableRowHover: {
+    //     '&:hover': {
+    //         backgroundColor: theme.palette.grey[200],
+    //     },
+    // },
+    //
+    // tableCell: {
+    //     flex: 1,
+    // },
+    // noClick: {
+    //     cursor: 'initial',
+    // },
+    paper: {padding: 5, marginTop: 5, marginBottom: 5},
 });
 
 const style = {
-    Paper: {padding: 5, marginTop: 5, marginBottom: 5}
+    Paper: {padding: 5, marginTop: 5, marginBottom: 5},
+
 }
 
 // const tableStyle = {
@@ -92,30 +100,33 @@ class OperationList extends React.Component<any, any> {
                 <div>
 
                     <Paper>
-                        <Grid container={true} direction="column" spacing={8} wrap={"nowrap"}>
+                        <Grid container={true} direction="column" spacing={8}>
                             {/*<Grid container={true} direction="row" justify="flex-start" alignItems="flex-start">*/}
-                            <Grid container={true} item={true} wrap={"nowrap"}>
-                                <Paper style={style.Paper}>
-                                    <Typography variant="h5">Current Balance: {this.props.currentBalance}</Typography>
-                                </Paper>
+                            <Grid container={true} item={true} spacing={8} xs={6}>
+                                <Grid item={true} xs={3}>
+                                    <Paper className={classes.paper}>
+                                        <Typography align={"left"} noWrap={true} variant="h6">Current
+                                            Balance:</Typography>
+                                    </Paper>
+                                </Grid>
+                                <Grid item={true} xs={3}>
+                                    <Paper className={classes.paper} color={"#00cc00"}>
+                                        <Typography align={"left"} noWrap={true} variant="h6"
+                                                    color={"primary"}>{this.displayNumber(this.props.currentBalance)}</Typography>
+                                    </Paper>
+                                </Grid>
                             </Grid>
                             <Grid container={true} className={"demo"} direction="row" justify="flex-start"
-                                  alignItems="center" spacing={16} item={true}>
-                                <Grid item={true}>
-                                    <Paper style={style.Paper}>
-                                        <Typography variant="h5">Period: </Typography>
+                                  alignItems="flex-start" spacing={8} item={true} xs={6}>
+                                <Grid item={true} xs={3}>
+                                    <Paper className={classes.paper}>
+                                        <Typography align={"left"} variant="h6">Period: </Typography>
                                     </Paper>
                                 </Grid>
-                                <Grid item={true}>
-                                    <Paper style={style.Paper}>
-                                        <Typography variant="h5">From: </Typography>
-                                        <Typography variant="h5">{this.displayDate(this.props.startPeriod)}</Typography>
-                                    </Paper>
-                                </Grid>
-                                <Grid item={true}>
-                                    <Paper style={style.Paper}>
-                                        <Typography variant="h5">To: </Typography>
-                                        <Typography variant="h5">{this.displayDate(this.props.endPeriod)}</Typography>
+                                <Grid item={true} xs={3}>
+                                    <Paper className={classes.paper}>
+                                        <Typography align={"left"}
+                                                    variant="h6">{this.displayDate(this.props.startPeriod)} - {this.displayDate(this.props.endPeriod)}</Typography>
                                     </Paper>
                                 </Grid>
                             </Grid>
@@ -123,31 +134,31 @@ class OperationList extends React.Component<any, any> {
 
                             <Grid item={true}>
                                 <div style={{height: 500, 'overflowY': 'auto'}}>
-                                    <Table >
-                                        <TableHead >
+                                    <Table className={classes.table}>
+                                        <TableHead>
                                             <TableRow>
-                                                <TableCell align="center"><Typography
+                                                <TableCell className={classes.tableCell} align="center"><Typography
                                                     variant="h6">Date</Typography></TableCell>
-                                                <TableCell align="center"><Typography
+                                                <TableCell className={classes.tableCell} align="center"><Typography
                                                     variant="h6">Description</Typography></TableCell>
-                                                <TableCell align="right"><Typography
+                                                <TableCell className={classes.tableCell} align="right"><Typography
                                                     variant="h6">Amount</Typography></TableCell>
                                             </TableRow>
                                         </TableHead>
 
-                                        <TableBody >
+                                        <TableBody>
 
                                             {operations.map((operation: any) =>
 
-                                                <TableRow key={operation.id}>
-                                                    <TableCell component="th" scope="row">
+                                                <TableRow key={operation.id} className={classes.tableRow}>
+                                                    <TableCell className={classes.tableCell} component="th" scope="row">
                                                         <Typography
                                                             variant="body1">{this.displayDate(operation.date)}</Typography>
                                                     </TableCell>
-                                                    <TableCell align="right">
+                                                    <TableCell className={classes.tableCell} align="right">
                                                         <Typography variant="body1">{operation.description}</Typography>
                                                     </TableCell>
-                                                    <TableCell align="right">
+                                                    <TableCell className={classes.tableCell} align="right">
                                                         <Typography variant="body1">{operation.amount}</Typography>
                                                     </TableCell>
                                                 </TableRow>
@@ -160,12 +171,12 @@ class OperationList extends React.Component<any, any> {
                             </Grid>
                             <Grid container={true} item={true} spacing={8}>
                                 <Grid item={true}>
-                                    <Paper style={style.Paper}>
+                                    <Paper className={classes.paper}>
                                         <Typography variant="h5"> Initial Balance: </Typography>
                                     </Paper>
                                 </Grid>
                                 <Grid item={true}>
-                                    <Paper style={style.Paper}>
+                                    <Paper className={classes.paper}>
                                         <Typography variant="h5"> {this.props.initialBalance} </Typography>
                                     </Paper>
                                 </Grid>
@@ -186,12 +197,16 @@ class OperationList extends React.Component<any, any> {
         return (value !== undefined) ?
             new Intl.DateTimeFormat('en-GB', {
                 day: '2-digit',
-                month: 'long',
-                year: 'numeric'
+                month: 'short'
             }).format(new Date(value)) : "";
+    }
+
+    private displayNumber(value) {
+        return (value !== undefined) ?
+            new Intl.NumberFormat('en-GB', {maximumFractionDigits: 2}).format(value) : "0.00";
     }
 
 
 }
 
-export default OperationList;
+export default withStyles(styles)(OperationList);
