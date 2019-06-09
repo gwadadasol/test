deacf3ab71d1f9028bd676717c0010aecd3b2c14
pavcoding <gwadadasol@gmail.com>
2019-06-10 06:11:36 +0800

    Add a view component to display the total number in the database

--- csharp/OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
+++ csharp/OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -73,6 +73,11 @@ namespace OdeToFood.Data
             return restaurant;
             
         }
+
+        public int GetCountOfRestaurants()
+        {
+            return restaurants.Count();
+        }
     }
 
 }
