8449965588bccc54ae9c25eae5d2690ec1e577f5
Philippe ANSELME-VATIN <pavtocode@gmail.com>
2023-10-09 11:18:11 -0400

    create platform api

---
+++ csharp/microservicescourse/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,32 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController: ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            System.Console.WriteLine("--> Getting Plaforms...");
+
+            var platformItem = _repository.GetAllPlaforms();
+
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+    }
+}
---
+++ csharp/microservicescourse/PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,33 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void CreatePlatform(Platform plat)
+        {
+            if (plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+        }
+
+        public IEnumerable<Platform> GetAllPlaforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id) => _context.Platforms.FirstOrDefault(p => p.Id == id);
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
